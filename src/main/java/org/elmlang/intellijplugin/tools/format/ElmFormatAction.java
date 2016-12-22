b7db06b979d6dbbe30de4c09feb44d9e435d87f2
Ian Phillips <ianp@ianp.org>
2016-12-22 15:29:20 +0000

    Disable action when not configured
    Also add installation instructions, although the link is kind of f-ugly
    at the moment, need to fiddle with the layout for the settings page.

--- src/main/java/org/elmlang/intellijplugin/tools/format/ElmFormatAction.java
+++ src/main/java/org/elmlang/intellijplugin/tools/format/ElmFormatAction.java
@@ -1,6 +1,8 @@
 package org.elmlang.intellijplugin.tools.format;
 
+import com.intellij.ide.BrowserUtil;
 import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
@@ -9,25 +11,56 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.event.HyperlinkEvent;
+import java.io.File;
 
 public class ElmFormatAction extends AnAction {
+
     @Override
     public void actionPerformed(AnActionEvent event) {
-        System.out.println(event);
         ElmFormatSettings settings = ServiceManager.getService(ElmFormatSettings.class);
-        if (settings.getExecutable() != null) {
-            Project project = event.getData(CommonDataKeys.PROJECT);
-            if (project != null) {
-                Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-                if (editor != null) {
-                    System.out.println("formatting with " + settings.getExecutable());
-                    ElmFormatHelper.format(editor.getDocument(), settings.getExecutable());
-                }
+        String exec = settings.getExecutable();
+        if (exec != null && new File(exec).canExecute()) {
+            Editor editor = editorForEvent(event);
+            if (editor != null) {
+                ElmFormatHelper.format(editor.getDocument(), exec);
             }
         } else {
-            String msg = "You need to configure the path to elm-format in the tool settings.";
-            Notifications.Bus.notify(new Notification("Elm Format", "Not Configured", msg, NotificationType.WARNING));
+            String msg = "You need to configure the path to elm-format in the <a href=\"#settings\">tool settings</a>. If you need to install elm-format instructions can be found <a href=\"#instructions\">here</a>";
+            Notifications.Bus.notify(
+                    new Notification("ElmFormat", "Elm Format Missing", msg, NotificationType.WARNING, new MyLinkListener()));
+        }
+    }
+
+    @Override
+    public void update(AnActionEvent event) {
+        ElmFormatSettings settings = ServiceManager.getService(ElmFormatSettings.class);
+        String exec = settings.getExecutable();
+        event.getPresentation().setEnabled(exec != null && new File(exec).canExecute() && editorForEvent(event) != null);
+    }
+
+    private Editor editorForEvent(AnActionEvent event) {
+        Project project = event.getData(CommonDataKeys.PROJECT);
+        return project != null ? FileEditorManager.getInstance(project).getSelectedTextEditor() : null;
+    }
+
+    private static class MyLinkListener extends NotificationListener.Adapter {
+        @Override
+        protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
+            switch (event.getDescription()) {
+                case "#settings":
+                    ShowSettingsUtil.getInstance().showSettingsDialog(null, ElmSettingsFormUI.class);
+                    break;
+                case "#instructions":
+                    BrowserUtil.browse("https://github.com/avh4/elm-format#installation-");
+                    break;
+                default:
+                    System.out.println(event.getDescription());
+            }
         }
     }
 }
--- src/main/java/org/elmlang/intellijplugin/tools/format/ElmSettingsFormUI.java
+++ src/main/java/org/elmlang/intellijplugin/tools/format/ElmSettingsFormUI.java
@@ -1,10 +1,12 @@
 package org.elmlang.intellijplugin.tools.format;
 
+import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.ui.components.labels.LinkLabel;
 import net.miginfocom.swing.MigLayout;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -15,7 +17,6 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.io.File;
 
 public class ElmSettingsFormUI implements SearchableConfigurable {
 
@@ -46,17 +47,21 @@ public class ElmSettingsFormUI implements SearchableConfigurable {
     @Override
     public JComponent createComponent() {
         JTextField textField = new JTextField(settings.getExecutable());
+        textField.setToolTipText("The path to the elm-format command");
 
         executable = new TextFieldWithBrowseButton(textField);
-        executable.setToolTipText("The path to the elm-format command");
         executable.addBrowseFolderListener(null, null, null, new FileChooserDescriptor(true, false, false, false, false, false));
         formatOnSave = new JCheckBox("Format on save", settings.isFormatOnSave());
 
         JPanel panel = new JPanel(new MigLayout());
         JLabel executableLabel = new JLabel("Executable");
-        executableLabel.setLabelFor(executable);
+        executableLabel.setLabelFor(textField);
+        LinkLabel<String> instructions = new LinkLabel<>("Installation instructions", null,
+                (label, data) -> BrowserUtil.browse(data), "https://github.com/avh4/elm-format#installation-");
         panel.add(executableLabel);
         panel.add(executable, "grow, wrap");
+        panel.add(new JLabel(""));
+        panel.add(instructions, "grow, wrap");
         panel.add(new JLabel(""));
         panel.add(formatOnSave);
         return panel;
@@ -70,10 +75,17 @@ public class ElmSettingsFormUI implements SearchableConfigurable {
 
     @Override
     public void apply() throws ConfigurationException {
-        File file = new File(executable.getText());
-        if (formatOnSave.isSelected() && !file.canExecute()) {
-            throw new IllegalStateException("You do not have permission to execute " + file.getName());
-        }
+// FIXME: according to the link below, this should display an error in the UI, but it  doesn't.
+// https://intellij-support.jetbrains.com/hc/en-us/community/posts/207554825-How-to-display-form-validation-errors-in-settings-panel
+//        File file = new File(executable.getText());
+//        if (formatOnSave.isSelected()) {
+//            if (!file.exists()) {
+//                throw new IllegalStateException("No such command: " + file.getName());
+//            }
+//            if (!file.canExecute()) {
+//                throw new IllegalStateException("You do not have permission to execute " + file.getName());
+//            }
+//        }
         settings.setExecutable(executable.getText());
         settings.setFormatOnSave(formatOnSave.isSelected());
     }
